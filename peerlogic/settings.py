0c305e65e7e20cc2a894c103f1cc26c1a9a66b55
Tom Boolean <tomboolean@gmail.com>
2020-12-07 19:11:37 -0700

    celery beat tasks

--- peerlogic/settings.py
+++ peerlogic/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_extensions",
-    "django_userforeignkey",
+    "django_celery_beat",
     "corsheaders",
     "core",
     "reminders",
