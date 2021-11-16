38e42dc9574377ebba8d35fbb896d6431b245e3a
Ana Tomboulian <ana@peerlogic.com>
2021-11-16 13:35:39 -0700

    agent engaged with

--- calls/models.py
+++ calls/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django_extensions.db.fields import ShortUUIDField
 
 from phonenumber_field.modelfields import PhoneNumberField
 
 from core.models import AuditTrailModel
-from calls.field_choices import CallConnectionTypes, CallDirectionTypes, EngagementPersonaTypes, NonAgentTypes, ReferralSourceTypes, TelecomPersonaTypes
+from calls.field_choices import (
+    CallConnectionTypes,
+    CallDirectionTypes,
+    EngagementPersonaTypes,
+    NonAgentEngagementPersonaTypes,
+    ReferralSourceTypes,
+    TelecomPersonaTypes,
+)
 
 
 class Call(AuditTrailModel):
@@ -15,7 +23,7 @@ class Call(AuditTrailModel):
     duration_seconds = models.DurationField()
     connect_duration_seconds = models.DurationField()
     progress_time_seconds = models.DurationField()
-    call_direction = models.CharField(choices=CallDirectionTypes.choices, max_length=50)
+    call_direction = models.CharField(choices=CallDirectionTypes.choices, max_length=50, db_index=True)
     domain = models.ForeignKey(
         "auth.Group",
         on_delete=models.SET_NULL,
@@ -34,11 +42,16 @@ class Call(AuditTrailModel):
     referral_source = models.CharField(choices=ReferralSourceTypes.choices, max_length=50, blank=True)
     caller_type = models.CharField(choices=EngagementPersonaTypes.choices, max_length=50)
     callee_type = models.CharField(choices=EngagementPersonaTypes.choices, max_length=50)
-    agent_engaged_with = models.CharField(choices=NonAgentTypes.choices, max_length=50, blank=True)
     metadata_file_uri = models.CharField(max_length=255)
 
 
+class AgentEngagedWith(models.Model):
+    id = ShortUUIDField(primary_key=True, editable=False)
+    call = models.ForeignKey("Call", on_delete=models.CASCADE, verbose_name="The call engaged with", related_name="engaged_in_calls")
+    non_agent_engagement_persona_type = models.CharField(choices=NonAgentEngagementPersonaTypes.choices, max_length=50, blank=True)
+
+
 class CallLabel(AuditTrailModel):
-    call = models.ForeignKey("Call", on_delete=models.CASCADE, verbose_name="The call being labeled", related_name="calls")
-    metric = models.CharField(max_length=255)
-    label = models.CharField(max_length=255)
+    call = models.ForeignKey("Call", on_delete=models.CASCADE, verbose_name="The call being labeled", related_name="labeled_calls")
+    metric = models.CharField(max_length=255, db_index=True)
+    label = models.CharField(max_length=255, db_index=True)
