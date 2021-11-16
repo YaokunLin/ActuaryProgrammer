daef1c24ab4a49c684c74fbace36e602ee99e310
Ana Tomboulian <ana@peerlogic.com>
2021-11-15 22:06:12 -0700

    wip of calls endpoint

--- calls/views.py
+++ calls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Call, CallLabel
+from .serializers import CallSerializer#, CallLabelSerializer
+
+class CallViewset(viewsets.ModelViewSet):
+    queryset = Call.objects.all()
+    serializer_class = CallSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+# class LabelViewset(viewsets.ModelViewSet):
+#     queryset = CallLabel.objects.all()
+#     serializer_class = CallLabelSerializer
