02b1c11bb12ed54676ff43fb007eb63e3c0b2306
Ana Tomboulian <ana@peerlogic.com>
2021-11-16 11:35:46 -0700

    /calls/ endpoint

--- calls/serializers.py
+++ calls/serializers.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.models import Group
+
 from rest_framework import serializers
 
-from core.models import GroupTelecom
 
 from .models import Call
 
 
 class CallSerializer(serializers.ModelSerializer):
-    domain = serializers.PrimaryKeyRelatedField(queryset=GroupTelecom.objects.all())    
+    domain = serializers.SlugRelatedField(queryset=Group.objects.all(), slug_field="name")    
 
     class Meta:
         model = Call
         fields = "__all__"
-        read_only_fields = ["id"]
+        read_only_fields = ["id", "created_at", "modified_by", "modified_at"]
