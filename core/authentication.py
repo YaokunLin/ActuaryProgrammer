a08aa278b830085e27961ab9b1671bf14b0c94c1
Ana Tomboulian <ana@peerlogic.com>
2021-11-17 12:39:15 -0700

    get access token request changes

--- core/authentication.py
+++ core/authentication.py
@@ -5,10 +5,9 @@ import requests
 
 from django.apps import apps as django_apps
 from django.conf import settings
-from django.utils.encoding import smart_text
 from django.utils.translation import ugettext as _
 from rest_framework.exceptions import AuthenticationFailed
-from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework.authentication import BaseAuthentication
 
 import xmltodict
 
@@ -22,7 +21,7 @@ class JSONWebTokenAuthentication(BaseAuthentication):
     def authenticate(self, request):
         """Entrypoint for Django Rest Framework"""
 
-        jwt_token = self.get_jwt_token(request)
+        jwt_token = self.get_auth_access_token_from_request(request)
 
         if jwt_token is None:
             return None
@@ -30,10 +29,8 @@ class JSONWebTokenAuthentication(BaseAuthentication):
         response = self.introspect_token(jwt_token)
         payload = xmltodict.parse(response)["Oauthtoken"]
 
-
         USER_MODEL = self.get_user_model()
         user = USER_MODEL.objects.get_or_create_from_token_payload(payload)
-        print(user)
         return (user, None)
 
     def introspect_token(self, token: str):
@@ -51,23 +48,22 @@ class JSONWebTokenAuthentication(BaseAuthentication):
     def get_user_model(self):
         return django_apps.get_model(settings.AUTH_USER_MODEL, require_ready=False)
 
-    def get_jwt_token(self, request):
-        auth = get_authorization_header(request).split()
-        if not auth or smart_text(auth[0].lower()) != "bearer":
-            return None
+    def get_auth_access_token_from_request(self, request_object) -> str:
+        """Obtain the access token from the appropriate header."""
 
-        if len(auth) == 1:
-            msg = _("Invalid Authorization header. No credentials provided.")
-            raise AuthenticationFailed(msg)
-        elif len(auth) > 2:
-            msg = _("Invalid Authorization header. Credentials string " "should not contain spaces.")
-            raise AuthenticationFailed(msg)
+        auth_header_name = "Authorization"
+        auth_header_value = request_object.headers.get(auth_header_name, None)
+        if not auth_header_value:
+            raise AuthenticationFailed(f"'{auth_header_name}' header is required")
 
-        return auth[1].decode()
+        auth_parts = auth_header_value.split(" ")
 
-    def authenticate_header(self, request):
-        """
-        Method required by the DRF in order to return 401 responses for authentication failures, instead of 403.
-        More details in https://www.django-rest-framework.org/api-guide/authentication/#custom-authentication.
-        """
-        return "Bearer: api"
+        if len(auth_parts) != 2:
+            raise AuthenticationFailed(f"'{auth_header_name}' header is invalid. Received: '{auth_header_value}'")
+
+        token_type, token = auth_parts
+        token_type_expected = "Bearer"
+        if token_type.lower() != token_type_expected.lower():
+            raise AuthenticationFailed(f"'{auth_header_name}' header must be a '{token_type_expected}' token")
+
+        return token
