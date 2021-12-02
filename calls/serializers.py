4bb6e0f3f5ebb43a3adf25ef143efad5fe50e020
Alex Maskovyak <alexander.maskovyak@gmail.com>
2021-12-02 16:38:37 -0700

    Remove lookup field since its the primary key now

--- calls/serializers.py
+++ calls/serializers.py
@@ -30,5 +30,4 @@ class TelecomCallerNameInfoSerializer(serializers.ModelSerializer):
         model = TelecomCallerNameInfo
         fields = "__all__"
         read_only_fields = ["id", "created_at", "modified_by", "modified_at"]
-        lookup_field = "phone_number"
      
