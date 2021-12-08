76dff6e7c6a889dd4235a078faabf94eb099a637
Alex Maskovyak <alexander.maskovyak@gmail.com>
2021-12-08 11:16:05 -0800

    Provide filtering for mobile_number and searching for first_name and last_name for contacts to support Velox and Desktop integration.

--- core/views.py
+++ core/views.py
@@ -20,4 +20,5 @@ class ContactViewset(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
-        
+    filterset_fields = ["mobile_number"]
+    search_fields = ["first_name", "last_name"]
