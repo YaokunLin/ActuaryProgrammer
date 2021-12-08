e16717d83564422b82a2f4fbb3a820d627553c22
Ana Tomboulian <ana@peerlogic.com>
2021-12-08 14:44:33 -0700

    black

---
+++ core/managers.py
@@ -0,0 +1,52 @@
+from django.apps import apps
+from django.db import models
+from django.db import IntegrityError
+from django.contrib.auth.models import UserManager as _UserManager
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+
+class PracticeManager(models.Manager):
+    use_in_migrations = True
+
+    def get_by_natural_key(self, name):
+        return self.get(name=name)
+
+
+class UserManager(_UserManager):
+    INTROSPECT_TOKEN_PAYLOAD_KEYS = ["token", "client_id", "territory", "domain", "uid", "expires", "scope", "mask_chain"]
+
+    def get_or_create_from_introspect_token_payload(self, payload):
+        self._validate_introspect_token_payload(payload)
+        uid = payload["uid"]
+
+        try:
+            user = self.get(username=uid)
+            user.last_login = timezone.now()
+            user.is_active = True
+            user.save()
+            return user
+        except self.model.DoesNotExist:
+            pass
+
+        # Set practice domain
+
+        Practice = apps.get_model("app_name", "ModelName")
+        practice, _ = Practice.objects.get_or_create(name=payload["domain"])
+
+        try:
+            user = self.create(
+                username=uid,
+                date_joined=timezone.now(),
+                last_login=timezone.now(),
+                is_active=True,
+            )
+            practice.user_set.add(user)
+        except IntegrityError:
+            user = self.get(username=uid)
+
+        return user
+
+    def _validate_introspect_token_payload(self, payload):
+        if not all(key in self.INTROSPECT_TOKEN_PAYLOAD_KEYS for key in payload):
+            raise ValueError("Wrong payload to get or create a user")
