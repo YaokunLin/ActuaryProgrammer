bc0346f318d71956ad52018470384d96f0dcaf6c
Chrissy Albert <chrissy@peerlogic.com>
2021-12-17 10:42:06 -0700

    change peerlogic id modelField, add velox extract data

--- etl/models.py
+++ etl/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-from core.models import Patient
+from django_extensions.db.fields import ShortUUIDField
 
 
 class VeloxExtractData(models.Model):
+    def velox_extract_data_default():
+        return {}
+
     velox_id = models.IntegerField(primary_key=True) # Velox id, not peerlogic-api id
-    peerlogic_id = models.CharField(max_length=255)
+    peerlogic_id = ShortUUIDField( editable=False)
     deleted = models.BooleanField()
     pms_id = models.IntegerField()
     first_name = models.CharField(max_length=255)
@@ -18,4 +21,4 @@ class VeloxExtractData(models.Model):
     zip = models.IntegerField()
     email = models.EmailField(max_length=254)
     status = models.IntegerField()
-
+    velox_extract_data = models.JSONField(default=velox_extract_data_default)
