172c642018df156fa0cb5e1a5f356878c82a30f4
Chrissy Albert <chrissy@peerlogic.com>
2021-12-20 09:26:42 -0700

    add filterset and search fields to views, change primary key to peerlogic_patient_id

--- etl/views.py
+++ etl/views.py
@@ -9,3 +9,6 @@ from .serializers import VeloxExtractDataSerializer
 class ETLViewset(viewsets.ModelViewSet):
     queryset = VeloxPatientExtract.objects.all()
     serializer_class = VeloxExtractDataSerializer
+    
+    filterset_fields = ["velox_id", "peerlogic_patient_id"]
+    search_fields = ["peerlogic_patient_id"]
