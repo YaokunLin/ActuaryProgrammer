03d5eb7ffc8b3a160b84d87d4a9758a76ce25cf7
Ana Tomboulian <ana@peerlogic.com>
2022-01-08 23:55:54 -0700

    list endpoint and cleanup

--- inbox/views.py
+++ inbox/views.py
@@ -1,6 +1,10 @@
+import logging
+
 from django.conf import settings
 from django.http import Http404
 
+
+from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,9 +19,12 @@ from .bandwidth.serializers import (
 from .models import SMSMessage
 from .serializers import SMSMessageSerializer
 
+# Get an instance of a logger
+log = logging.getLogger(__name__)
 
-class SMSMessageViewSet(viewsets.ModelViewSet):
-    queryset = SMSMessage.objects.all().order_by("-created_at")
+
+class SMSMessageList(generics.ListCreateAPIView):
+    queryset = SMSMessage.objects.all()
     serializer_class = SMSMessageSerializer
 
 
@@ -86,7 +93,6 @@ class SMSMessagesFailedCallbackView(APIView):
 class SMSMessagesView(APIView):
     """
     Create a new SMSMessage.
-    # TODO: list once Bandwidth callbacks are available
     # TODO: from_number will always come from 1 assigned group/domain's sms number, not willy-nilly
     """
 
@@ -95,30 +101,39 @@ class SMSMessagesView(APIView):
         create_message_serializer = CreateSMSMessageAndConvertToBandwidthRequestSerializer(data=request.data)
         create_message_serializer_is_valid = create_message_serializer.is_valid()
         if not create_message_serializer_is_valid:
-            return Response(create_message_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            log.exception(f"Error from create_message_serializer validation: {create_message_serializer.errors}")
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"errors": create_message_serializer.errors})
 
         # Call Telecom Client
         response = settings.BANDWIDTH_CLIENT.post(settings.BANDWIDTH_MESSAGING_URI, json=create_message_serializer.data)
         response_data = response.json()
-
         if response.status_code >= 300:
-            return Response(response_data, status=response.status_code)
+            log.exception(response_data)
+            return Response(status=response.status_code, data={"bandwidth_api_errors": response_data})
 
         # Convert Response to sms message
         bandwidth_response_to_sms_message = BandwidthResponseToSMSMessageSerializer(data=response_data)
         bandwidth_response_to_sms_message_is_valid = bandwidth_response_to_sms_message.is_valid()
         if not bandwidth_response_to_sms_message_is_valid:
-            return Response(bandwidth_response_to_sms_message.errors, status=status.HTTP_400_BAD_REQUEST)
+            log.exception(f"Error from bandwidth_response_to_sms_message validation: {bandwidth_response_to_sms_message.errors}")
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={"errors": bandwidth_response_to_sms_message.errors})
 
         sms_message = bandwidth_response_to_sms_message.save()
         serializer = SMSMessageSerializer(sms_message)
 
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(status=status.HTTP_202_ACCEPTED, data=serializer.data)
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.username for sms_message in SMSMessage.objects.all()]
+        return Response(usernames)
 
 
 class SMSMessageDetail(APIView):
     """
-    Retrieve or update a SMSMessage instance.
+    Retrieve or update an SMSMessage instance.
     """
 
     def get_object(self, pk):
