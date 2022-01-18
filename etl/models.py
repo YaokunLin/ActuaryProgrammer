992fa680d9d4cbaefc34dd5ec49e9d9f090e9e85
Ana Tomboulian <ana@peerlogic.com>
2022-01-18 16:38:13 -0700

    remove db_index

--- etl/models.py
+++ etl/models.py
@@ -6,12 +6,12 @@ from core.abstract_models import AuditTrailModel
 
 
 class NetsapiensCdrsExtract(AuditTrailModel):
-    netsapiens_callid = models.CharField(primary_key=True, max_length=127, db_index=True)
+    netsapiens_callid = models.CharField(primary_key=True, max_length=127)
     peerlogic_call_id = models.CharField(blank=True, default="", max_length=22, db_index=True)
     netsapiens_cdrs_extract = models.JSONField(default=dict, help_text="CDRs")
 
 
 class VeloxPatientExtract(AuditTrailModel):
-    velox_id = models.IntegerField(primary_key=True, db_index=True)
+    velox_id = models.IntegerField(primary_key=True)
     peerlogic_patient_id = models.CharField(blank=True, default="", max_length=22, db_index=True)
     velox_patient_extract_data = models.JSONField(default=dict)
