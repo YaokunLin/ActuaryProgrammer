c6646d6ef15c3e86d305623704555b7dbeb72a94
Ana Tomboulian <ana@peerlogic.com>
2022-01-18 16:57:14 -0700

    allow any - not worky

--- etl/views.py
+++ etl/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from .models import NetsapiensCdrsExtract, VeloxPatientExtract
 from .serializers import NetsapiensCdrsExtractSerializer, VeloxPatientExtractSerializer
 
 
 class NetsapiensCdrsExtractViewset(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     queryset = NetsapiensCdrsExtract.objects.all()
     serializer_class = NetsapiensCdrsExtractSerializer
 
