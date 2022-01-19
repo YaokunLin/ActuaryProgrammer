64f8f534af319a2628029044e6fd8e8dc05c0702
Ana Tomboulian <ana@peerlogic.com>
2022-01-18 17:39:13 -0700

    subscriptions

--- etl/views.py
+++ etl/views.py
@@ -1,10 +1,33 @@
-from django.shortcuts import render
+import logging
 
 from rest_framework import viewsets
+from rest_framework.decorators import (api_view, authentication_classes,
+                                       permission_classes)
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 from .models import NetsapiensCdrsExtract, VeloxPatientExtract
-from .serializers import NetsapiensCdrsExtractSerializer, VeloxPatientExtractSerializer
+from .serializers import (NetsapiensCdrsExtractSerializer,
+                          VeloxPatientExtractSerializer)
+
+# Get an instance of a logger
+log = logging.getLogger(__name__)
+
+
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def netsapiens_call_subscription_view(request):
+    log.info(f"Netsapiens Call subscription: {request.data}")
+    return Response(request.data)
+
+
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([AllowAny])
+def netsapiens_call_origid_subscription_view(request):
+    log.info(f"Netsapiens Call ORIG id subscription: {request.data}")
+    return Response(request.data)
 
 
 class NetsapiensCdrsExtractViewset(viewsets.ModelViewSet):
