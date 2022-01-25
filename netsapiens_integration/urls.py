24bf0d7f1cffe77b8acc6b462382cc4852c764bc
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-01-24 16:30:36 -0800

    Removing netsapiens prefixes. Adding admin functionality.

--- netsapiens_integration/urls.py
+++ netsapiens_integration/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from .views import (
+    AdminNetsapiensAPICredentialsViewset,
     netsapiens_call_origid_subscription_view,
     netsapiens_call_subscription_view,
     NetsapiensAPICredentialsViewset,
@@ -12,8 +13,9 @@ app_name = "netsapiens_integration"
 router = routers.DefaultRouter()
 
 
-router.register(r"netsapiens-subscription-clients", NetsapiensSubscriptionClientViewset, basename="netsapiens-subscription-clients")
-router.register(r"netsapiens-api-credentials", NetsapiensAPICredentialsViewset)
+router.register(r"subscription-clients", NetsapiensSubscriptionClientViewset, basename="netsapiens-subscription-clients")
+router.register(r"api-credentials", NetsapiensAPICredentialsViewset)
+router.register(r"admin/api-credentials", AdminNetsapiensAPICredentialsViewset)
 
 
 urlpatterns = [
