a8cdcfcaf4e63929208f5f6e45e05c253a729e38
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-01-25 12:32:01 -0700

    Add api credentials to admin.

--- netsapiens_integration/admin.py
+++ netsapiens_integration/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from netsapiens_integration.models import NetsapiensCallsSubscriptionEventExtract, NetsapiensSubscriptionClient
+from netsapiens_integration.models import (
+    NetsapiensAPICredentials,
+    NetsapiensCallsSubscriptionEventExtract,
+    NetsapiensSubscriptionClient,
+)
 
+admin.site.register(NetsapiensAPICredentials)
 admin.site.register(NetsapiensSubscriptionClient)
 admin.site.register(NetsapiensCallsSubscriptionEventExtract)
