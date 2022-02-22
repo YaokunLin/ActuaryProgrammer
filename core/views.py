21071be8760291cee39cbe977cfb10e310af3482
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-02-22 13:41:15 -0800

    Reorder imports

--- core/views.py
+++ core/views.py
@@ -4,8 +4,8 @@ from typing import Dict
 from django.conf import settings
 from django.db import transaction
 from django.http import (
-    HttpResponseBadRequest,
     HttpRequest,
+    HttpResponseBadRequest,
 )
 from django.utils import timezone
 from pydantic import BaseModel
