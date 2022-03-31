52a9a1088d1146d22a939b57ee7f9425ff24b2dc
Atom Boolean <ana@peerlogic.com>
2022-03-31 11:23:36 -0700

    need __id at the end for filters

--- calls/analytics/intents/views.py
+++ calls/analytics/intents/views.py
@@ -7,16 +7,16 @@ from calls.analytics.intents.serializers import CallOutcomeSerializer, CallOutco
 class CallPurposeViewset(viewsets.ModelViewSet):
     queryset = CallPurpose.objects.all().order_by("-modified_at")
     serializer_class = CallPurposeSerializer
-    filter_fields = ["call_purpose_type", "call"]
+    filter_fields = ["call_purpose_type", "call__id"]
 
 
 class CallOutcomeViewset(viewsets.ModelViewSet):
     queryset = CallOutcome.objects.all().order_by("-modified_at")
     serializer_class = CallOutcomeSerializer
-    filter_fields = ["call_outcome_type", "call_purpose", "call_purpose__call"]
+    filter_fields = ["call_outcome_type", "call_purpose__id", "call_purpose__call__id"]
 
 
 class CallOutcomeReasonViewset(viewsets.ModelViewSet):
     queryset = CallOutcomeReason.objects.all().order_by("-modified_at")
     serializer_class = CallOutcomeReasonSerializer
-    filter_fields = ["call_outcome_reason_type", "call_outcome", "call_outcome__call_purpose", "call_outcome__call_purpose__call"]
+    filter_fields = ["call_outcome_reason_type", "call_outcome__id", "call_outcome__call_purpose__id", "call_outcome__call_purpose__call__id"]
