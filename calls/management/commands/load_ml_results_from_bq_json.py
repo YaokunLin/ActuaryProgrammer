2f016f7a4bd60a5f2bf7fed2307b62a831452b09
Atom Boolean <ana@peerlogic.com>
2022-07-26 16:16:08 -0700

    call purpose, outcome and outcome reason

--- calls/management/commands/load_ml_results_from_bq_json.py
+++ calls/management/commands/load_ml_results_from_bq_json.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Dict
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -7,12 +8,21 @@ from django.core.management.base import BaseCommand
 from oauth2_provider.models import (
     get_application_model,
 )
-from calls.analytics.intents.models import CallMentionedInsurance, CallMentionedProcedure, CallMentionedProduct, CallMentionedSymptom
+from calls.analytics.intents.models import (
+    CallMentionedInsurance,
+    CallMentionedProcedure,
+    CallMentionedProduct,
+    CallMentionedSymptom,
+    CallOutcome,
+    CallOutcomeReason,
+    CallPurpose,
+)
 from calls.analytics.interactions.models import AgentCallScore
 from calls.analytics.transcripts.models import CallSentiment
 from calls.field_choices import SentimentTypes
 from calls.models import Call
 from core.models import Practice
+from ml.models import MLModelResultHistory
 
 Application = get_application_model()
 
@@ -63,6 +73,8 @@ class Command(BaseCommand):
 
         call_sentiment = call.pop("call_sentiment")
         call_agent_interactions = call.pop("call_agent_interaction")
+
+        # Mentioned
         call_company_mentioned = call.pop("call_company_discussed")
         call_insurance_mentioned = call.pop("call_insurance_discussed")
         call_procedure_mentioned = call.pop("call_procedure_discussed")
@@ -102,8 +114,9 @@ class Command(BaseCommand):
         self._load_insurance_mentioned(call_insurance_mentioned, peerlogic_api_call)
         self._load_product_mentioned(call_product_mentioned, peerlogic_api_call)
         self._load_symptom_mentioned(call_symptom_mentioned, peerlogic_api_call)
+        self._load_call_purposes_and_outcomes_with_outcome_reasons(call_purpose, peerlogic_api_call)
 
-    def _load_call_sentiment(self, call_sentiment, call):
+    def _load_call_sentiment(self, call_sentiment: Dict, call: Call) -> None:
         BQ_TO_PEERLOGIC_API_SENTIMENT_TYPE_MAP = {
             "positive": SentimentTypes.POSITIVE,
             "negative": SentimentTypes.NEGATIVE,
@@ -126,7 +139,7 @@ class Command(BaseCommand):
         else:
             self.stdout.write(f"Found call sentiment with pk={peerlogic_api_call_sentiment.pk}")
 
-    def _load_agent_call_scores(self, call_agent_interactions, call):
+    def _load_agent_call_scores(self, call_agent_interactions: Dict, call: Call) -> None:
 
         for interaction in call_agent_interactions:
             metric = interaction["agent_interaction_metric_name"]
@@ -140,7 +153,7 @@ class Command(BaseCommand):
             else:
                 self.stdout.write(f"Found agent call score with pk={peerlogic_api_agent_call_score.pk}")
 
-    def _load_procedure_mentioned(self, call_procedure_mentioned, call):
+    def _load_procedure_mentioned(self, call_procedure_mentioned: Dict, call: Call) -> None:
 
         for procedure in call_procedure_mentioned:
             keyword = procedure["call_procedure_keyword"]
@@ -156,7 +169,7 @@ class Command(BaseCommand):
             else:
                 self.stdout.write(f"Found call mentioned procedure with pk={peerlogic_api_call_mentioned_procedure.pk}")
 
-    def _load_company_mentioned(self, call_company_mentioned, call):
+    def _load_company_mentioned(self, call_company_mentioned: Dict, call: Call) -> None:
 
         for company in call_company_mentioned:
             keyword = company["call_company_keyword"]
@@ -172,7 +185,7 @@ class Command(BaseCommand):
             else:
                 self.stdout.write(f"Found call mentioned company with pk={peerlogic_api_call_mentioned_company.pk}")
 
-    def _load_insurance_mentioned(self, call_insurance_mentioned, call):
+    def _load_insurance_mentioned(self, call_insurance_mentioned: Dict, call: Call) -> None:
 
         for insurance in call_insurance_mentioned:
             keyword = insurance["call_insurance_keyword"]
@@ -188,7 +201,7 @@ class Command(BaseCommand):
             else:
                 self.stdout.write(f"Found call mentioned insurance with pk={peerlogic_api_call_mentioned_insurance.pk}")
 
-    def _load_symptom_mentioned(self, call_symptom_mentioned, call):
+    def _load_symptom_mentioned(self, call_symptom_mentioned: Dict, call: Call) -> None:
 
         for symptom in call_symptom_mentioned:
             keyword = symptom["call_symptom_keyword"]
@@ -204,7 +217,7 @@ class Command(BaseCommand):
             else:
                 self.stdout.write(f"Found call mentioned symptom with pk={peerlogic_api_call_mentioned_symptom.pk}")
 
-    def _load_product_mentioned(self, call_product_mentioned, call):
+    def _load_product_mentioned(self, call_product_mentioned: Dict, call: Call) -> None:
 
         for product in call_product_mentioned:
             keyword = product["call_product_keyword"]
@@ -219,3 +232,72 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.SUCCESS(f"Created call mentioned product with pk='{peerlogic_api_call_mentioned_product.pk}'"))
             else:
                 self.stdout.write(f"Found call mentioned product with pk={peerlogic_api_call_mentioned_product.pk}")
+
+    def _load_call_purposes_and_outcomes_with_outcome_reasons(self, call_purposes: Dict, call: Call) -> None:
+
+        for bq_call_purpose in call_purposes:
+            peerlogic_api_call_purpose = self._load_call_purpose(bq_call_purpose, call)
+            peerlogic_api_call_outcome = self._load_call_outcome(bq_call_purpose, peerlogic_api_call_purpose)
+            self._load_call_outcome_reason(bq_call_purpose, peerlogic_api_call_outcome)
+
+    def _load_call_purpose(self, call_purpose: Dict, call: Call) -> CallPurpose:
+        call_purpose_type = call_purpose["purpose"]
+        if not call_purpose_type:
+            return
+
+        defaults = {"raw_call_purpose_model_run_id": call_purpose["purpose_run_id"]}
+
+        self.stdout.write(f"Getting or creating Call Purpose with call_purpose_type='{call_purpose_type}'.")
+        peerlogic_api_call_purpose, created = CallPurpose.objects.get_or_create(
+            pk=call_purpose["call_purpose_id"], call=call, call_purpose_type=call_purpose_type, defaults=defaults
+        )
+        if created:
+            self.stdout.write(self.style.SUCCESS(f"Created call purpose with pk='{peerlogic_api_call_purpose.pk}'"))
+        else:
+            self.stdout.write(f"Found call purpose with pk={peerlogic_api_call_purpose.pk}")
+
+        return peerlogic_api_call_purpose
+
+    def _load_call_outcome(self, call_purpose: Dict, peerlogic_api_call_purpose: CallPurpose) -> CallOutcome:
+        call_outcome_type = call_purpose["outcome"]
+        if not call_outcome_type:
+            return
+
+        defaults = {
+            # keep the model run id the same as the purpose run id because they come from
+            # the same model.
+            "raw_call_outcome_model_run_id": call_purpose["purpose_run_id"]
+        }
+
+        self.stdout.write(f"Getting or creating Call Outcome with call_outcome_type='{call_outcome_type}'.")
+        peerlogic_api_call_outcome, created = CallOutcome.objects.get_or_create(
+            call_purpose=peerlogic_api_call_purpose, call_outcome_type=call_outcome_type, defaults=defaults
+        )
+        if created:
+            self.stdout.write(self.style.SUCCESS(f"Created call outcome with pk='{peerlogic_api_call_outcome.pk}'"))
+        else:
+            self.stdout.write(f"Not creating - Found existing call outcome with pk={peerlogic_api_call_outcome.pk}")
+
+        return peerlogic_api_call_outcome
+
+    def _load_call_outcome_reason(self, call_purpose: Dict, peerlogic_api_call_outcome: CallOutcome) -> CallOutcomeReason:
+        call_outcome_reason_type = call_purpose["outcome_reason"]
+        if not call_outcome_reason_type:
+            return
+
+        defaults = {
+            # keep the model run id the same as the purpose run id because they come from
+            # the same model.
+            "raw_call_outcome_reason_model_run_id": call_purpose["purpose_run_id"]
+        }
+
+        self.stdout.write(f"Getting or creating Call Outcome Reason with call_outcome_reason_type='{call_outcome_reason_type}'.")
+        peerlogic_api_call_outcome_reason, created = CallOutcomeReason.objects.get_or_create(
+            call_outcome=peerlogic_api_call_outcome, call_outcome_reason_type=call_outcome_reason_type, defaults=defaults
+        )
+        if created:
+            self.stdout.write(self.style.SUCCESS(f"Created call outcome reason with pk='{peerlogic_api_call_outcome_reason.pk}'"))
+        else:
+            self.stdout.write(f"Not creating - Found existing call outcome reason with pk={peerlogic_api_call_outcome_reason.pk}")
+
+        return peerlogic_api_call_outcome_reason
