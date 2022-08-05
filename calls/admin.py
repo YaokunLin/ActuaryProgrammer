04ecc14a86c6c199b4df0adbb54352d2c35f9cb9
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-08-05 09:49:12 -0700

    Add all models for call to admin

--- calls/admin.py
+++ calls/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
 from .models import (
+    Call,
+    CallAudio,
+    CallAudioPartial,
+    CallLabel,
+    CallPartial,
+    CallTranscript,
+    CallTranscriptPartial,
     TelecomCallerNameInfo,
 )
 
+admin.site.register(Call)
+admin.site.register(CallAudio)
+admin.site.register(CallAudioPartial)
+admin.site.register(CallLabel)
+admin.site.register(CallPartial)
+admin.site.register(CallTranscript)
+admin.site.register(CallTranscriptPartial)
 admin.site.register(TelecomCallerNameInfo)
