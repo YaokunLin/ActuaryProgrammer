a55c35700dc935dc2dcf2d07a9b1b34631a85efa
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-08-07 11:30:12 -0700

    Move analytics field choices to the normal field choices endpoint

--- calls/analytics/views.py
+++ calls/analytics/views.py
@@ -5,12 +5,19 @@ from calls.analytics.intents.field_choices import (
     CallOutcomeReasonTypes,
     CallPurposeTypes,
 )
-from calls.analytics.participants.field_choices import EngagementPersonaTypes, NonAgentEngagementPersonaTypes
+from calls.analytics.interactions.field_choices import (
+    AgentInteractionMetricTypes,
+)
+from calls.analytics.participants.field_choices import (
+    EngagementPersonaTypes,
+    NonAgentEngagementPersonaTypes,
+)
 
 
 class CallAnalyticsFieldChoicesView(views.APIView):
     def get(self, request, format=None):
         result = {}
+        result["agent_interaction_metric_types"] = dict((str(y), x) for x, y in AgentInteractionMetricTypes.choices)  # retain as choices / non-dynamic for now, keys MUST be cast as strings here
         result["engagement_persona_types"] = dict((y, x) for x, y in EngagementPersonaTypes.choices)
         result["non_agent_engagement_persona_types"] = dict((y, x) for x, y in NonAgentEngagementPersonaTypes.choices)
         result["call_purpose_types"] = dict((y, x) for x, y in CallPurposeTypes.choices)
