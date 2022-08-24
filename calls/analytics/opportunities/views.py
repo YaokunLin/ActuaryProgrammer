2a60dc2374a3c89132f6c58a151802fb85379888
Atom Boolean <ana@peerlogic.com>
2022-08-24 11:13:54 -0700

    practice filter

--- calls/analytics/opportunities/views.py
+++ calls/analytics/opportunities/views.py
@@ -13,22 +13,53 @@ from calls.models import (
     Call,
 )
 from calls.validation import call_date_validation
+from core.models import Agent
 
 
 # Get an instance of a logger
 log = logging.getLogger(__name__)
 
 
+def practice_id_validation(request):
+    practice__id = request.query_params.get("practice__id")
+
+    if not practice__id:
+        return False, {"practice__id": "Must include practice id for call analytics."}
+
+    if request.user.is_staff or request.user.is_superuser:
+        return True, None
+
+    # Can see any practice's resources if you are an assigned agent to a practice
+    allowed_practice_ids = Agent.objects.filter(user=request.user.id).values("practice__id")
+    if practice__id not in allowed_practice_ids:
+        return False, {"practice__id": f"Not allowed to see resources with practice__id='{practice__id}'"}
+
+    return True, None
+
+
 class NewPatientWinbacksView(views.APIView):
     QUERY_FILTER_TO_HUMAN_READABLE_DISPLAY_NAME = {"call_start_time__gte": "call_start_time_after", "call_start_time__lte": "call_start_time_before"}
 
     def get(self, request, format=None):
-        # date filters
+        practice__id = request.query_params.get("practice__id")
+        valid_practice, practice_errors = practice_id_validation(request=request)
         dates_info = call_date_validation(query_data=request.query_params)
-        errors = dates_info.get("errors")
+        dates_errors = dates_info.get("errors")
+
+        errors = {}
+        if practice_errors:
+            errors.update(practice_errors)
+        if dates_errors:
+            errors.update(dates_errors)
         if errors:
             return Response(status=status.HTTP_400_BAD_REQUEST, data=errors)
 
+        # practice filter
+        practice_filter = {}
+        if valid_practice and practice__id:
+            practice_filter = {"practice__id": practice__id}
+
+        # date filters
         dates = dates_info.get("dates")
         call_start_time__gte = dates[0]
         call_start_time__lte = dates[1]
@@ -40,21 +71,24 @@ class NewPatientWinbacksView(views.APIView):
             call_direction=CallDirectionTypes.INBOUND,
             engaged_in_calls__non_agent_engagement_persona_type=NonAgentEngagementPersonaTypes.NEW_PATIENT,
             **dates_filter,
+            **practice_filter,
         )
         winback_opportunities_total_qs = new_patient_opportunities_qs.filter(
-            call_purposes__outcome_results__call_outcome_type=CallOutcomeTypes.FAILURE, **dates_filter
+            call_purposes__outcome_results__call_outcome_type=CallOutcomeTypes.FAILURE, **dates_filter, **practice_filter
         )
         winback_opportunities_won_qs = Call.objects.filter(
             call_direction=CallDirectionTypes.OUTBOUND,
             engaged_in_calls__non_agent_engagement_persona_type=NonAgentEngagementPersonaTypes.NEW_PATIENT,
             call_purposes__outcome_results__call_outcome_type=CallOutcomeTypes.SUCCESS,
             **dates_filter,
+            **practice_filter,
         )
         winback_opportunities_lost_qs = Call.objects.filter(
             call_direction=CallDirectionTypes.OUTBOUND,
             engaged_in_calls__non_agent_engagement_persona_type=NonAgentEngagementPersonaTypes.NEW_PATIENT,
             call_purposes__outcome_results__call_outcome_type=CallOutcomeTypes.FAILURE,
             **dates_filter,
+            **practice_filter,
         )
 
         aggregates["new_patient_opportunities_total"] = new_patient_opportunities_qs.count()
