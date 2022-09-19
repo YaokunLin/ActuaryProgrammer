e0910a2bc98bde32d59e729d81e91d4c1cf41fda
Atom Boolean <ana@peerlogic.com>
2022-09-19 10:41:11 -0700

    fix missing import

--- jive_integration/views.py
+++ jive_integration/views.py
@@ -13,7 +13,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from rest_framework.decorators import api_view, authentication_classes, permission_classes, renderer_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
