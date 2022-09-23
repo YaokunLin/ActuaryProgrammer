052a604d6986d394d9ca76acae96652e0e57a4ad
Ian Powell <ian@hardindd.com>
2022-09-22 23:52:41 -0500

    Small cleanups

--- ringcentral_integration/views.py
+++ ringcentral_integration/views.py
@@ -1,7 +1,6 @@
 import json
 import logging
 
-from django.shortcuts import render
 from django.db import transaction
 from django.http import HttpResponse, HttpRequest
 from rest_framework import viewsets
@@ -14,13 +13,12 @@ from ringcentral import SDK
 from ringcentral_integration.publishers import publish_call_discounted_event
 from django.utils import timezone
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.exceptions import (
     APIException,
-    AuthenticationFailed,
     ParseError,
     PermissionDenied,
 )
