6fff7cce44ecc71113f355c41504af4e46992c8b
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-09-22 17:06:21 -0700

    Make AEW a one-to-one relationship and unique

--- calls/analytics/participants/models.py
+++ calls/analytics/participants/models.py
@@ -1,24 +1,26 @@
 from django.db import models
 from django_extensions.db.fields import ShortUUIDField
 
-
 from calls.analytics.participants.field_choices import NonAgentEngagementPersonaTypes
 from core.abstract_models import AuditTrailModel
 from core.models import Agent
 
+
 class AgentAssignedCall(AuditTrailModel):
     id = ShortUUIDField(primary_key=True, editable=False)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE, related_name="assigned_calls")
-    call = models.ForeignKey("Call", on_delete=models.CASCADE, verbose_name="Call assigned to agent", related_name="assigned_agent") 
+    call = models.ForeignKey("Call", on_delete=models.CASCADE, verbose_name="Call assigned to agent", related_name="assigned_agent")
 
 
 class AgentEngagedWith(AuditTrailModel):
     id = ShortUUIDField(primary_key=True, editable=False)
     # TODO: rename this related name 'engaged_in_calls' to something that makes more sense
-    call = models.ForeignKey("Call", on_delete=models.CASCADE, verbose_name="The call engaged with", related_name="engaged_in_calls")
+    call = models.ForeignKey(
+        "Call", unique=True, on_delete=models.CASCADE, verbose_name="The call engaged with", related_name="engaged_in_calls"
+    )  # TODO: remove unique constraint and make history tables
     non_agent_engagement_persona_type = models.CharField(choices=NonAgentEngagementPersonaTypes.choices, max_length=50, blank=True)
-    
-    # Blankable - Sometimes we determine type outside of running a model and it's all app-side logic. For instance Business CNAM.
+
+    # Blankable - Sometimes we determine type outside of running a model, and it's all app-side logic. For instance, Business CNAM lookup instead of a model
     raw_non_agent_engagement_persona_model_run_id = models.CharField(max_length=22, blank=True)
     non_agent_engagement_persona_model_run = models.ForeignKey(
         "ml.MLModelResultHistory",
