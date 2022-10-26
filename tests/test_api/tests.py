bae18febf166b61027e59bd649f5c82c23029fe5
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-10-26 09:33:58 -0700

    Rename the tests directory to be lowercase and standard

--- tests/test_api/tests.py
+++ tests/test_api/tests.py
@@ -3,13 +3,16 @@ import requests
 from django.test import TestCase
 
 # Create your tests here.
-peerlogic = requests.post()
+peerlogic = requests.post()  # TODO: need to add code to configure url
+
 
 def test_api():
     pass
 
+
 @pytest.fixture
 def function_fixture():
     pass
-    
+
+
 # TODO: Implement
