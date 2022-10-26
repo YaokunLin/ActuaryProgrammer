14294fd67b67a26db4d82114618fe4545304eec3
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-10-26 15:47:55 -0700

    Fix formatting. Perform requests inside the test function

--- tests/test_api/tests.py
+++ tests/test_api/tests.py
@@ -2,17 +2,18 @@ import os
 import pytest
 import requests
 
+
 # Setting environment variables
-os.environ['LOCAL_URL'] = 'http://0.0.0.0:8000/'
+os.environ["LOCAL_URL"] = "http://0.0.0.0:8000/"
 
 # Getting environment variables
-LOCAL = os.getenv('LOCAL_URL')
+LOCAL = os.getenv("LOCAL_URL")
 
-# Create your tests here.
-peerlogic = requests.get(LOCAL)  # TODO: need to add code to configure url
 
 def test_api():
+    peerlogic = requests.get(LOCAL)  # TODO: need to add code to configure url
     assert peerlogic.status_code==200
+
 
 @pytest.fixture
 def function_fixture():
