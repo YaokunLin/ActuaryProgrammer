2466498af96a934969f5c2426885277aba55f42f
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-10-27 09:47:19 -0700

    Remove comment which a typo

--- tests/test_api/tests.py
+++ tests/test_api/tests.py
@@ -6,7 +6,6 @@ import requests
 from peerlogic.settings import TEST_API_CLIENT_ROOT_URL
 
 
-# Create you tet cases here
 def test_api():
     peerlogic = requests.get(TEST_API_CLIENT_ROOT_URL)
     assert peerlogic.status_code == 200
