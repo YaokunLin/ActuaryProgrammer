6e1189ca8aa6128afdf88bf70ac6f795b81ce8d6
JoshuaPaste <110432961+JoshuaPaste@users.noreply.github.com>
2022-10-27 17:05:53 -0700

    Addressed Alex comment and made test_api and e2e test
    (didn't mean to make this a second commit)

--- tests/test_api/tests.py
+++ tests/test_api/tests.py
@@ -4,7 +4,6 @@ import requests
 from peerlogic.settings import TEST_API_CLIENT_ROOT_URL
 
 
-@pytest.mark.unit
 def test_api():
     peerlogic = requests.get(TEST_API_CLIENT_ROOT_URL)
     assert peerlogic.status_code == 200
