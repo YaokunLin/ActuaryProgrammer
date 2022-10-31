461f4a052aa18798b29ad44be9469fc847430981
JoshuaPaste <110432961+JoshuaPaste@users.noreply.github.com>
2022-10-31 16:11:21 -0700

    Fixed naming convention, turned back to a unit test

--- tests/test_api/tests.py
+++ tests/test_api/tests.py
@@ -2,7 +2,7 @@ import pytest
 
 from tests.test_api.factories import LIST_OF_FAKE_PRACTICES
 
-
-def test_for_active_practices():  # TODO- make a unit test case
+@pytest.mark.unit
+def test_for_practices():  # TODO- make a unit test case
     for pract in LIST_OF_FAKE_PRACTICES:
         assert pract.name == pract.name
