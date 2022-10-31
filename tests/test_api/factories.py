6f4df775e5b500b915c76ab1ac4200614d6a9dc7
JoshuaPaste <110432961+JoshuaPaste@users.noreply.github.com>
2022-10-31 16:27:07 -0700

    Pointed at Practice class instead of using fake practices  class

--- tests/test_api/factories.py
+++ tests/test_api/factories.py
@@ -1,22 +1,17 @@
 from faker import Faker
+from core.models import Practice
 
 fake = Faker()
 counter = 0
-class FakePractice:
-    def __init__(self,id, name, industry, active):
-        self.id = id
-        self.name = name
-        self.industry = industry
-        self.active = active
 
 LIST_OF_FAKE_PRACTICES = []
 
-def FakerMakerPractices(Class:FakePractice, list,):
-    fake_Pract = FakePractice(fake.random_int(1000, 2000), f"{fake.first_name()}'s Dential",
+def FakerMakerPractices(Class:Practice, list,):
+    fake_Pract = Practice(fake.random_int(1000, 2000), f"{fake.first_name()}'s Dential",
     industry = "Dential", active = fake.boolean(chance_of_getting_true=80))
     list.append(fake_Pract)
 
 while counter < 5:
-    FakerMakerPractices(FakePractice, LIST_OF_FAKE_PRACTICES)
+    FakerMakerPractices(Practice, LIST_OF_FAKE_PRACTICES)
     counter += 1
 
