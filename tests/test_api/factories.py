1daef147dee105a7557437013c53f19bf85a16bd
JoshuaPaste <110432961+JoshuaPaste@users.noreply.github.com>
2022-11-02 11:23:55 -0700

    made assigning values to variables consistent

--- tests/test_api/factories.py
+++ tests/test_api/factories.py
@@ -5,7 +5,7 @@ from faker import Faker
 from core.models import Practice
 
 fake = Faker()
-fake_Practice = Practice(shortuuid.uuid(), f"{fake.first_name()}'s Dental", industry="Dental", active=fake.boolean(chance_of_getting_true=80))
+fake_Practice = Practice(shortuuid.uuid(), f"{fake.first_name()}'s Dental", "Dental", fake.boolean(chance_of_getting_true=80))
 
 
 class PracticeModelFactory(factory.django.DjangoModelFactory):
