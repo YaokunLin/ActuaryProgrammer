0d29dcc469751e3b230aa4e6e13ae1072be979cb
JoshuaPaste <110432961+JoshuaPaste@users.noreply.github.com>
2022-11-02 12:28:20 -0700

    finished and fully integrated factory to build fake practices
    before the code was just adding the fake practice defined at the beginning to the list, and not using the factory or build. It is now fully working and I have a much better understanding of how factory works.

--- tests/test_api/factories.py
+++ tests/test_api/factories.py
@@ -5,17 +5,16 @@ from faker import Faker
 from core.models import Practice
 
 fake = Faker()
-fake_Practice = Practice(shortuuid.uuid(), f"{fake.first_name()}'s Dental", "Dental", fake.boolean(chance_of_getting_true=80))
 
 
 class PracticeModelFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Practice
-        django_get_or_create = fake_Practice
+
+    id = shortuuid.uuid()
+    name = f"{fake.first_name()}'s Dental"
+    industry = "Dental"
+    active = fake.boolean(chance_of_getting_true=80)
 
 
 build = PracticeModelFactory.build_batch(3)
-
-LIST_OF_FAKE_PRACTICES = []
-
-LIST_OF_FAKE_PRACTICES.append(fake_Practice)
--- tests/test_api/tests.py
+++ tests/test_api/tests.py
@@ -1,9 +1,9 @@
 import pytest
 
-from tests.test_api.factories import LIST_OF_FAKE_PRACTICES
+from tests.test_api.factories import build
 
 
 @pytest.mark.unit
-def test_for_practices():  # TODO- make a unit test case
-    for practice in LIST_OF_FAKE_PRACTICES:
+def test_for_practices():
+    for practice in build:
         assert practice.name == practice.name
