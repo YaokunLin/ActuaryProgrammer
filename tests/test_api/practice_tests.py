524bca432f807baac870ee90a778b177876cfcc8
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-11-02 18:32:29 -0700

    Rename practice test file to indicate it tests practices

--- tests/test_api/practice_tests.py
+++ tests/test_api/practice_tests.py
@@ -3,7 +3,7 @@ import pytest
 from tests.test_api.factories import build
 
 
-@pytest.mark.unit
+# @pytest.mark.unit
 def test_for_practices():
     for practice in build:
         assert practice.name == practice.name
