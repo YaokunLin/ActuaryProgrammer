4ce11bf0584076242bd3fe808369d12f9cd7fd5f
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-11-02 18:22:44 -0700

    Fix first e2e test

--- tests/test_api/e2e_tests.py
+++ tests/test_api/e2e_tests.py
@@ -2,7 +2,7 @@ import requests
 
 from peerlogic.settings import TEST_API_CLIENT_ROOT_URL
 
-response = requests.get(TEST_API_CLIENT_ROOT_URL)
+response = requests.get(f"{TEST_API_CLIENT_ROOT_URL}/admin")
 
 
 def test_api():
