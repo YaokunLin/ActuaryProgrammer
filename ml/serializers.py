66cee8458fbfaae5f8b854b4296fdd9d6e8acd17
Kyle Amos <kyleamos@Kyles-MacBook-Pro.local>
2022-11-04 16:09:15 -0400

    Adjustments to ML record endpoints

--- ml/serializers.py
+++ ml/serializers.py
@@ -1,5 +1,8 @@
+from django.utils import timezone
 from rest_framework import serializers
 
+from calls.models import Call
+from ml.field_choices import ClassificationDomain
 from ml.models import MLModel, MLModelResultHistory
 
 
@@ -16,4 +19,26 @@ class MLModelResultHistorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MLModelResultHistory
         fields = "__all__"
-        read_only_fields = ["id", "created_by", "created_at", "modified_by", "modified_at"]
+        read_only_fields = ["id", "created_by", "created_at", "modified_by", "modified_at", "call_id", "model_id", "run_id", "label", "score", "threshold"]
+
+
+class MLModelResultHistoryCreateSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True)
+    created_at = serializers.DateTimeField(required=False, allow_null=False, default=timezone.now())
+    classification_domain = serializers.ChoiceField(choices=ClassificationDomain.choices, required=True, allow_null=False, allow_blank=False)
+    call_id = serializers.PrimaryKeyRelatedField(queryset=Call.objects.all(), pk_field=serializers.CharField())
+    model_id = serializers.PrimaryKeyRelatedField(queryset=MLModel.objects.all(), pk_field=serializers.CharField())
+    run_id = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_length=22, min_length=22)
+    label = serializers.CharField(required=True, allow_null=False, allow_blank=False)
+    score = serializers.FloatField(required=True, allow_null=True)
+    threshold = serializers.FloatField(required=True, allow_null=True)
+
+    def to_internal_value(self, data: dict) -> dict:
+        deserialized = super().to_internal_value(data)
+        for field in {"call_id", "model_id"}:
+            deserialized[field] = deserialized[field].id
+        return deserialized
+
+
+class MLModelResultHistoryBulkCreateSerializer(serializers.Serializer):
+    records = MLModelResultHistoryCreateSerializer(many=True)
--- ml/views.py
+++ ml/views.py
@@ -1,9 +1,19 @@
-from rest_framework import viewsets, views
+from typing import Any
+
+from rest_framework import exceptions, views, viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser
+from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 
 from ml.field_choices import ClassificationDomain
 from ml.models import MLModel, MLModelResultHistory
-from ml.serializers import MLModelResultHistorySerializer, MLModelSerializer
+from ml.serializers import (
+    MLModelResultHistoryBulkCreateSerializer,
+    MLModelResultHistorySerializer,
+    MLModelSerializer,
+)
 
 
 class MLFieldChoicesView(views.APIView):
@@ -14,12 +24,30 @@ class MLFieldChoicesView(views.APIView):
 
 
 class MLModelViewset(viewsets.ModelViewSet):
+    permission_classes = (IsAdminUser,)
     queryset = MLModel.objects.all().order_by("-modified_at")
     serializer_class = MLModelSerializer
     filter_fields = ["classification_domain", "model_version"]
 
 
-class MLModelResultHistoryViewset(viewsets.ModelViewSet):
+class MLModelResultHistoryViewset(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (IsAdminUser,)
     queryset = MLModelResultHistory.objects.all().order_by("-modified_at")
     serializer_class = MLModelResultHistorySerializer
     filter_fields = ["classification_domain", "call__id", "model__id", "run_id", "label", "score"]
+    http_method_names = ["head", "options", "get", "post"]
+
+    @action(detail=False, methods=["post"])
+    def bulk(self, request: Request) -> Response:
+        serializer = MLModelResultHistoryBulkCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        created_objects = MLModelResultHistory.objects.bulk_create(
+            [MLModelResultHistory(**r) for r in serializer.validated_data["records"]], ignore_conflicts=True
+        )
+
+        # Dumbed down serializer here since we don't want to do any unnecessary DB shenanigans
+        output_serializer = MLModelResultHistoryBulkCreateSerializer({"records": created_objects})
+        return Response(status=HTTP_201_CREATED, data=output_serializer.data)
+
+    def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        raise exceptions.MethodNotAllowed(method=request.method)
