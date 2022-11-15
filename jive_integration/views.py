a86fbf7b3c60165abd0f27ca1a82b9ade51e3e9b
Ian Powell <ian@hardindd.com>
2022-11-15 17:16:28 -0500

    More cleanup

--- jive_integration/views.py
+++ jive_integration/views.py
@@ -34,7 +34,7 @@ from core.validation import (
     get_validated_practice_telecom,
 )
 from jive_integration.field_choices import JiveEventTypeChoices
-from jive_integration.jive_client.client import JiveClient, Line
+from jive_integration.jive_client.client import JiveClient
 from jive_integration.models import (
     JiveAWSRecordingBucket,
     JiveChannel,
