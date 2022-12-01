a4b9ddde129a02ee941bba0502e0700a5c9691c8
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-12-01 14:08:03 -0700

    isort and black netsapient_integration

--- netsapiens_integration/management/commands/create_peerlogic_resources.py
+++ netsapiens_integration/management/commands/create_peerlogic_resources.py
@@ -1,8 +1,7 @@
+import requests
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 from django.db import connection
-
-import requests
 
 from core.models import Practice, PracticeTelecom, VoipProvider
 from netsapiens_integration.models import NetsapiensCallSubscription
