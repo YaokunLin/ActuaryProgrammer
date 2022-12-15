0774bc0c760c79ece0d2ae9426194c6625f3e6fb
Kyle Amos <kyleamos@Kyles-MacBook-Pro.local>
2022-12-15 14:18:43 -0500

    WIP

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ Cloud function entrypoints
 
 All we should do here is import entrypoints we wish to expose
 """
+import logging
+
+log = logging.getLogger(__name__)
+
 
 # noqa
-from cloud_functions.foo.entrypoint import foo
+# TODO Kyle: from cloud_functions.foo.entrypoint import foo
+def foo(event, context) -> None:
+    log.info(f"Started foo! Event: {event}, Context: {context}")
+    log.info("Completed foo!")
