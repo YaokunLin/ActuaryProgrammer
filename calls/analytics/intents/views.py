25d6b667e1b9089c0b8ff90c237559981b43ed68
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-12-21 18:03:41 -0800

    Re-order

--- calls/analytics/intents/views.py
+++ calls/analytics/intents/views.py
@@ -32,56 +32,8 @@ from calls.analytics.intents.serializers import (
 )
 from calls.models import Call
 
-
-class CallPurposeViewset(viewsets.ModelViewSet):
-    queryset = CallPurpose.objects.all().order_by("-modified_at")
-    serializer_class = CallPurposeSerializer
-    filter_fields = ["call_purpose_type", "call__id"]
-
-    def get_queryset(self):
-        queryset = super().get_queryset().filter(call_id=self.kwargs.get("call_pk"))
-        return queryset
-
-    def create(self, request, *args, **kwargs):
-        # overridden to provide the ability to perform single or multiple object creation
-        serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
-        serializer.is_valid(raise_exception=True)
-
-        with transaction.atomic():
-            # get call to update
-            call: Call = Call.objects.get(pk=self.kwargs.get("call_pk"))
-
-            # delete existing CallPurpose objects
-            call_purposes = call.call_purposes.all()
-            if call_purposes.exists():
-                call_purposes.delete()
-
-            # create the new / replacement CallPurpose objects
-            self.perform_create(serializer)
-
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-
-class CallOutcomeViewset(viewsets.ModelViewSet):
-    queryset = CallOutcome.objects.all().order_by("-modified_at")
-    serializer_class = CallOutcomeSerializer
-    filter_fields = ["call_outcome_type", "call_purpose__id", "call_purpose__call__id"]
-
-    def get_queryset(self):
-        # TODO: probably nest outcomes underneath a purpose
-        queryset = super().get_queryset().filter(call_purpose__call_id=self.kwargs.get("call_pk"))
-        return queryset
-
-
-class CallOutcomeReasonViewset(viewsets.ModelViewSet):
-    queryset = CallOutcomeReason.objects.all().order_by("-modified_at")
-    serializer_class = CallOutcomeReasonSerializer
-    filter_fields = ["call_outcome_reason_type", "call_outcome__id", "call_outcome__call_purpose__id", "call_outcome__call_purpose__call__id"]
-
-
 #
-# Mentioned Viewsets
+# Mixins
 #
 
 
@@ -137,6 +89,63 @@ class CallParentQueryFilterMixin:
     def get_queryset(self):
         queryset = super().get_queryset().filter(call=self.kwargs.get("call_pk"))
         return queryset
+
+
+#
+# Purpose and related
+#
+
+
+class CallPurposeViewset(viewsets.ModelViewSet):
+    queryset = CallPurpose.objects.all().order_by("-modified_at")
+    serializer_class = CallPurposeSerializer
+    filter_fields = ["call_purpose_type", "call__id"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(call_id=self.kwargs.get("call_pk"))
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        # overridden to provide the ability to perform single or multiple object creation
+        serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
+        serializer.is_valid(raise_exception=True)
+
+        with transaction.atomic():
+            # get call to update
+            call: Call = Call.objects.get(pk=self.kwargs.get("call_pk"))
+
+            # delete existing CallPurpose objects
+            call_purposes = call.call_purposes.all()
+            if call_purposes.exists():
+                call_purposes.delete()
+
+            # create the new / replacement CallPurpose objects
+            self.perform_create(serializer)
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class CallOutcomeViewset(viewsets.ModelViewSet):
+    queryset = CallOutcome.objects.all().order_by("-modified_at")
+    serializer_class = CallOutcomeSerializer
+    filter_fields = ["call_outcome_type", "call_purpose__id", "call_purpose__call__id"]
+
+    def get_queryset(self):
+        # TODO: probably nest outcomes underneath a purpose
+        queryset = super().get_queryset().filter(call_purpose__call_id=self.kwargs.get("call_pk"))
+        return queryset
+
+
+class CallOutcomeReasonViewset(viewsets.ModelViewSet):
+    queryset = CallOutcomeReason.objects.all().order_by("-modified_at")
+    serializer_class = CallOutcomeReasonSerializer
+    filter_fields = ["call_outcome_reason_type", "call_outcome__id", "call_outcome__call_purpose__id", "call_outcome__call_purpose__call__id"]
+
+
+#
+# Mentioned Viewsets
+#
 
 
 class CallMentionedCompanyViewset(CallParentQueryFilterMixin, CallParentDeleteChildrenOnCreateMixin, viewsets.ModelViewSet):
