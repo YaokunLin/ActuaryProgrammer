8729446e76b1de740dfa52af7847bb4b7224571f
Kyle Amos <kyleamos@Kyles-MacBook-Pro.local>
2023-01-31 17:47:15 -0500

    Comment out SMS endpoints for now

--- inbox/urls.py
+++ inbox/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("sms-messages/delivered-callback", views.SMSMessagesDeliveredCallbackView.as_view()),
-    path("sms-messages/failed-callback", views.SMSMessagesFailedCallbackView.as_view()),
-    path("sms-messages/<pk>", views.SMSMessageDetail.as_view()),
-    path("sms-messages/", views.SMSMessagesView.as_view()),
-    path("sms-messages", views.SMSMessageList.as_view()),
+    # path("sms-messages/delivered-callback", views.SMSMessagesDeliveredCallbackView.as_view()),
+    # path("sms-messages/failed-callback", views.SMSMessagesFailedCallbackView.as_view()),
+    # path("sms-messages/<pk>", views.SMSMessageDetail.as_view()),
+    # path("sms-messages/", views.SMSMessagesView.as_view()),
+    # path("sms-messages", views.SMSMessageList.as_view()),
 ]
